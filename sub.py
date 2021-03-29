a45fa8da85f74a2022be44176b4b82a1c261c27c
Yash <yashgoswamichamp@gmail.com>
2021-03-29 11:25:36 +0530

    bugs are solved

--- sub.py
+++ sub.py
@@ -1,5 +1,5 @@
-n1=input("input 1st Number")
-n2=input("input 2nd Number")
+n1=int(input("input 1st Number"))
+n2=int(input("input 2nd Number"))
 def sub1(n1,n2):
     return n1-n2
 print("Sum of two number is:",sub1(n1,n2))
