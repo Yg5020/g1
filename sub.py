be86bfcb0173d48133072fd3e55bbea4cd797fd9
Yash <yashgoswamichamp@gmail.com>
2021-03-29 11:37:43 +0530

    second comit

--- sub.py
+++ sub.py
@@ -2,4 +2,4 @@ n1=int(input("input 1st Number"))
 n2=int(input("input 2nd Number"))
 def sub1(n1,n2):
     return n1-n2
-print("Sum of two number is:",sub1(n1,n2))
+print("Sum of two numbers is:",sub1(n1,n2))
