89b20cd189cb7c7504e7144db01f0ada67a5e055
Yash <yashgoswamichamp@gmail.com>
2021-03-29 11:15:15 +0530

    first commit

---
+++ sub.py
@@ -0,0 +1,5 @@
+n1=input("input 1st Number")
+n2=input("input 2nd Number")
+def sub1(n1,n2):
+    return n1-n2
+print("Sum of two number is:",sub1(n1,n2))
---
+++ sum.py
@@ -0,0 +1,5 @@
+n1=int(input("input 1st Number"))
+n2=int(input("input 2nd Number"))
+def sum(n1,n2):
+    return n1+n2
+print("Sum of two number is:",sum(n1,n2))
