0d8eb4d578dea544ac525c716f224080a1ceea90
Yash <yashgoswamichamp@gmail.com>
2021-03-29 11:21:43 +0530

    bugs are fixed

--- sum.py
+++ sum.py
@@ -1,5 +1,7 @@
-n1=int(input("input 1st Number"))
-n2=int(input("input 2nd Number"))
+n1=input("input 1st Number")
+n2=input("input 2nd Number")
+n11=int(n1)
+n22=int(n2)
 def sum(n1,n2):
     return n1+n2
-print("Sum of two number is:",sum(n1,n2))
+print("Sum of two number is:",sum(n11,n22))
